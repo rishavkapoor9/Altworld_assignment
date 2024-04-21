e24eb730257902432b7c243b4a3f085effce16dc
Rishav Kapoor <rishavkapoorrk@gmail.com>
2024-04-21 10:10:40 +0530

    issues fixed

--- app/Context/context.tsx
+++ app/Context/context.tsx
@@ -1,9 +1,9 @@
 'use client'
-import { createContext, useContext, Dispatch, SetStateAction, useState } from "react";
+import { createContext, useContext, Dispatch, SetStateAction, useState, ReactNode } from "react";
 
 interface ContextProps{
     assId: String,
-    setAssId: Dispatch<SetStateAction<String>>,
+    setAssId: Dispatch<SetStateAction<string>>,
     userId: number,
     setUserId: Dispatch<SetStateAction<number>>,
 
@@ -12,14 +12,14 @@ interface ContextProps{
 
 const GlobalContext= createContext<ContextProps>({
     assId:  '',
-    setAssId: (): String=>  '',
+    setAssId: (): string=>  '',
     userId:  0,
     setUserId: (): number=> 0,
 
 
 })
 
-export const GlobalContextProvider = ({children})=>{
+export const GlobalContextProvider = ({ children }: { children: ReactNode })=>{
     const [assId, setAssId] = useState('')
     const [userId, setUserId] = useState(0)
 
